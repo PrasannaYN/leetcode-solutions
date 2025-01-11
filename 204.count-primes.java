4077ebca68588b4ef089747024226e902ea0ae94
Prasanna <gitprasanna@gmail.com>
2025-01-11 15:52:29 +0530

    count-primes

---
+++ 204.count-primes.java
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=204 lang=java
+ *
+ * [204] Count Primes
+ *
+ * https://leetcode.com/problems/count-primes/description/
+ *
+ * algorithms
+ * Medium (34.24%)
+ * Likes:    8165
+ * Dislikes: 1491
+ * Total Accepted:    982.4K
+ * Total Submissions: 2.9M
+ * Testcase Example:  '10'
+ *
+ * Given an integer n, return the number of prime numbers that are strictly
+ * less than n.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: n = 10
+ * Output: 4
+ * Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: n = 0
+ * Output: 0
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: n = 1
+ * Output: 0
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= n <= 5 * 10^6
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+
+        int count = 0;
+        boolean[] isPrime = new boolean[n];
+
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+// @lc code=end
+
